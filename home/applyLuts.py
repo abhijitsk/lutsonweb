4f5654af8307caace70cac58dfed91c7a10597d2
Abhijit Sasikuumar <abhijit.cusat@gmail.com>
2024-07-07 04:38:52 +0530

    logic on luts applied

---
+++ home/applyLuts.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageFilter
+import os
+
+listofLuts = os.listdir('home/LUTS')
+
+class ApplyLuts():
+    def __init__(self, image):
+        self.unfilteredimage = Image.open(image)
+
+    #listofLuts = os.listdir('home/LUTS')
+    def saveFilteredImage(self,path):
+        lut_table = []
+        luts_size=None
+        
+        with open('home/LUTS/'+path,'r') as cube:
+            for line in cube:
+                line=line.strip()
+                if not line or line.startswith('#') or line.startswith('TITLE'):
+                    continue
+                if line.startswith('LUT_3D_SIZE'):
+                    parts=line.split()
+                    if len(parts)==2:
+                        luts_size=int(parts[1])
+                    else:
+                        raise ValueError(" Luts Size is not defined properly")
+                elif luts_size is not None:
+                    lut_table.append((line))
+            row2val = lambda row: tuple([float(val) for val in row])
+            lutTable =[row2val(row.split(" ")) for row in lut_table]
+            filter = ImageFilter.Color3DLUT(luts_size,lutTable)
+            filtetredImage = self.unfilteredimage.filter(filter)
+            filtetredImage.save('media/output/'+path+'.jpeg')
+                
+        
+    
+    def ShowImages(self):
+        for luts in listofLuts:
+            self.saveFilteredImage(luts)
+        
+    
--- lutsonweb/settings.py
+++ lutsonweb/settings.py
@@ -126,3 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL ="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+
+
+#css static file from ecommerce
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
